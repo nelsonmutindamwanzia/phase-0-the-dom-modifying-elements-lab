575e5a60e0c667262b0776bc65a8b0d9f9114818
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-19 16:28:29 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.id;
+newHeader.innerHTML = "MwanziaNelsonMutinda is the champion";
